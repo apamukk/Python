2abd37ce9c608299df950be91acf181ead9192d0
Nikolay kovalev <kovalev.nikolay@ais-ais.kz>
2021-07-27 17:26:02 +0500

    Выгрузка первого задания

---
+++ Урок1/Задание1.py
@@ -0,0 +1,8 @@
+number1 = 13
+number2 = 15
+print(number1,number2)
+# играем с переменными
+name = input("Как Вас зовут?")
+print('Привет', name)
+age = input("сколько Вам лет?")
+print(name,'тебе',age,'лет')
